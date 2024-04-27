da30c725756ad70fc8d9ece27706a45fc39de4f6
souhardya2774 <souhardya2774@gmail.com>
2024-04-28 00:26:27 +0530

    Debug11

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -2,7 +2,5 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: "https://keep-notes-server-gamma.vercel.app",
-    withCredentials: true,
-    headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'},
-	credentials: 'include'
+    withCredentials: true
 });
